7bedc2ac24b2716fd05fbc92b8fd68b95b0a494e
mauflore <mauflore@bancolombia.com.co>
2024-03-06 22:19:41 -0500

    correcciones en el login

--- HealthWizard-web-app/src/App.js
+++ HealthWizard-web-app/src/App.js
@@ -8,17 +8,45 @@ import Login from './components/Login'
 import Recover from './components/Recover'
 import muscleWizard from './muscle_wizard.png';
 
+import React, { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+
 //importar router
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// importar librerias visuales
+import { Container } from 'react-bootstrap';
+//mport { doSignOut } from './firebaseConfig/auth';
 
 // *** function app de ejemplo ****
 // * Se modificó en App.css el estilo de la clase App-header para que ocupara menos espacio en pantalla
 function App() {
+
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => { //manejo de sesión activa
+      setUser(user);
+    });
+
+    return () => unsubscribe(); // Limpia el observador cuando el componente se desmonta
+  }, []); // Se ejecuta solo una vez al montar el componente
+
+  const doSignOut = async (e) => {
+    e.preventDefault()
+    await signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    })
+  };
+
   return (
     <div className="App">
       
-      <header className="App-header"> 
-        {/* <img src={logo} className="App-logo" alt="logo" style={{ height: "120px"}} />
+       <header className="App-header"> 
+        {/*<img src={logo} className="App-logo" alt="logo" style={{ height: "120px"}} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -32,21 +60,27 @@ function App() {
         </a>
 
         <h3 style={{ padding: "10px"}}>Hello Firebase, I'm App Health Wizard</h3>
-        <button className="btn btn-danger">I'm useless!</button> */}
+        <button className="btn btn-danger">I'm useless!</button>*/}
         <img src={muscleWizard} alt="Muscle Wizard" className="app-symbol"/>						
         <h1 className="app-title">Health Wizard</h1>
-
-      </header>
-
+        {user ? (
+        <p>El usuario está autenticado: {user.email} <a href="#" onClick={doSignOut}>Cerrar sesión</a></p>        
+        ) : (
+          <p>No hay usuario autenticado</p>
+        )}
+      </header> 
+      
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={ <Login />} />
-          <Route path='/register' element={ <Register />} />
-          <Route path='/recover' element={ <Recover />} />
-          <Route path='/create' element={ <Create />} />
-          <Route path='/edit/:id' element={ <Edit />} />
-          <Route path='/show' element={ <Show />} />          
-        </Routes>
+        <Container fluid> {/* Utiliza Container fluid de Bootstrap */}
+          <Routes>
+            <Route path='/' element={ <Login />} />
+            <Route path='/register' element={ <Register />} />
+            <Route path='/recover' element={ <Recover />} />
+            <Route path='/create' element={ <Create />} />
+            <Route path='/edit/:id' element={ <Edit />} />
+            <Route path='/show' element={ <Show />} />          
+          </Routes>
+        </Container>
       </BrowserRouter>
       
     </div>
